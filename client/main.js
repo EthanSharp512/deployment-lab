49d0b9580fdf167a9a527c958853d55c36cf7e37
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-18 16:57:04 -0600

    another update to the res data not sending on roll dice function

--- client/main.js
+++ client/main.js
@@ -1,12 +1,11 @@
 const rollDiceBtn = document.querySelector('#dice-roll');
-const rollDiceInput = document.getElementById()
 
 
 const rollDice = () => {
     axios.get("/api/rolldice")
     .then(res => {
         const data = res.data;
-        document.getElementById('dice-roll-output').textContent = data;
+        document.getElementById('dice-roll-output').textContent = data.value;
     });
 };
 
