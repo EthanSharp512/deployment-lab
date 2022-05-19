cc661e8af58d0b19ea48f30fcbdec0890090e8d9
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-19 00:37:30 -0600

    dicemessage

--- server/controller.js
+++ server/controller.js
@@ -1,19 +1,23 @@
-let rolls = []
+let equal = true;
 
 module.exports = {
     rollDice: (req, res) => {
         const throwdice1 =  Math.floor(Math.random() * 6) + 1;
         const throwdice2 =  Math.floor(Math.random() * 6) + 1;
+        let rolls = []
         rolls.push(throwdice1, throwdice2)
+        if(rolls[0]===rolls[1]) {
+            let equal = true;
+        } else{
+            let equal = false;
+        }
         res.status(200).send(rolls);
     },
     diceMessage: (req, res) => {
-        if(rolls[0] === rolls[1]){
-            let message = "A broken clock is right twice a day..."
-            res.status(200).send(message);
-        } else if(rolls[0] !== rolls[1]){
-            let message = "A fool and his life are soon parted..."
-            res.status(200).send(message);
+        if(equal === true){
+            res.status(200).send("You rolled doubles! A broken clock is right twice a day...");
+        } else if(equal === false){
+            res.status(200).send("You didn't roll doubles... A fool and his life are soon parted...");
         }
     }
 }
